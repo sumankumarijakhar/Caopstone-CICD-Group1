c3de2ef3315e3459d09523359b572f3eccec7595
sumankumarijakhar <Sjakhar5590@conestogac.on.ca>
2025-08-14 02:54:29 -0400

    app

--- bin/app.ts
+++ bin/app.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CapstoneStack } from '../lib/stack';
 
 const app = new cdk.App();
+
 new CapstoneStack(app, 'CapstoneStack', {
-  env: {
-    // Fill your AWS account and region if you want to pin:
-    // account: process.env.CDK_DEFAULT_ACCOUNT,
-    // region: process.env.CDK_DEFAULT_REGION
-  }
+  // Avoids the hnb659fds bootstrap requirement
+  synthesizer: new cdk.LegacyStackSynthesizer(),
+
+  // Optional: pin env if you want
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
